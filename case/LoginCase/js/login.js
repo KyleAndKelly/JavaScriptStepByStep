73d476a54c15a70b5b2da49078c889f85cd3483d
KyleAndKelly <2431173627@qq.com>
2024-10-23 00:04:06 +0900

    use Boostrap build HTML page

--- case/LoginCase/js/login.js
+++ case/LoginCase/js/login.js
@@ -1,22 +1,30 @@
-const username = "randomuser"
-const password = "123456"
-const p_element = document.querySelector('.my_p_1')
 
-axios({
-    url: 'http://hmajax.itheima.net/api/login',
-    method:'POST',
-    data:{
-        username,
-        password
-    }
-}).then(result => {
-    console.log(result.data.message)
-    if(p_element){
-        p_element.innerHTML = result.data.message
-    }
-}).catch(error=>{
-    console.log(error.response.data.message)
-    if(p_element){
-        p_element.innerHTML = error.response.data.message
-    }
-}) 
+const loginBtn = document.querySelector('.login-btn')
+loginBtn.addEventListener('click',function(){
+    event.preventDefault();
+    console.log("clicked")
+    const username = document.querySelector('.user').value  
+    const password = document.querySelector('.pwd').value  
+    console.log(username)
+    console.log(password)
+    const p = document.querySelector('.my_p_1')  
+    axios({
+        url: 'http://hmajax.itheima.net/api/login',
+        method:'POST',
+        data:{
+            username,
+            password
+        }
+    }).then(result => {
+        console.log(result.data.message)
+        if(p){
+            p.innerHTML = result.data.message
+        }
+    }).catch(error=>{
+        console.log(error.response.data.message)
+        if(p){
+            p.innerHTML = error.response.data.message
+        }
+    }) 
+      
+})
