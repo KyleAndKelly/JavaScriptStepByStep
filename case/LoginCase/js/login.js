ddecf544325e4c2389ddc3d11ea1982ca862c0c1
KyleAndKelly <2431173627@qq.com>
2024-10-24 23:36:31 +0900

    add message box

--- case/LoginCase/js/login.js
+++ case/LoginCase/js/login.js
@@ -1,5 +1,21 @@
 
 const loginBtn = document.querySelector('.login-btn')
+const myAlert = document.querySelector('.alert')
+function alert (msg,success){
+    myAlert.classList.remove('d-none');
+    myAlert.innerText = msg
+    myAlert.classList.add('show')
+    myAlert.classList.add(success? 'alert-success':'alert-danger')
+
+    setTimeout(()=> {
+        myAlert.classList.add('d-none');
+        myAlert.innerText = ''
+        myAlert.classList.remove('show')
+        myAlert.classList.remove(success? 'alert-success':'alert-danger')    
+    },1000)
+    
+}
+
 loginBtn.addEventListener('click',function(){
     event.preventDefault();
     console.log("clicked")
@@ -17,14 +33,10 @@ loginBtn.addEventListener('click',function(){
         }
     }).then(result => {
         console.log(result.data.message)
-        if(p){
-            p.innerHTML = result.data.message
-        }
+        alert('Login Success!',1)
     }).catch(error=>{
         console.log(error.response.data.message)
-        if(p){
-            p.innerHTML = error.response.data.message
-        }
+        alert('Login Failed!',0)
     }) 
       
 })
